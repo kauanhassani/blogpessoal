4e8c8410a07f9fa4ce0382e78ca66389ef2f6f3b
kauanhassani <kauanhassani@gmail.com>
2023-06-27 15:28:12 -0300

    CRUD Parte 1

---
+++ src/main/java/com/generation/blogpessoal/model/Tema.java
@@ -0,0 +1,34 @@
+package com.generation.blogpessoal.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+/*
+@Entity
+@Table(name = "tb_tema")
+public class Tema {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	
+	@NotNull
+	private String descricao;
+	
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getDescricao() {
+		return this.descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+}
+*/
